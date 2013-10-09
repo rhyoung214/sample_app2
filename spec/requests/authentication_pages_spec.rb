bbcefdb0a7f48126a64cf070383f3122f896fe80
Robert <rhyoung214@gmail.com>
2013-10-09 15:04:06 -0400

    Ch 9 exercises

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -96,6 +96,36 @@ describe "Authentication" do
 					it {should have_title('Sign in') }
 				end
 			end
+
+			describe "when attempting to visit a protected page" do
+				before do
+					visit edit_user_path(user)
+					fill_in "Email",	with: user.email
+					fill_in "Password", with: user.password
+					click_button "Sign in"
+				end
+
+				describe "after signing in" do
+
+					it "should render the desired protected page" do
+						expect(page).to have_title('Edit user')
+					end
+
+					describe "when signing in again" do
+						before do
+							delete signout_path
+							visit signin_path
+							fill_in "Email",	with: user.email
+							fill_in "Password", with: user.password
+							click_button "Sign in"
+						end
+
+						it "should render the defualt (profile) page" do
+							expect(page).to have_title(user.name)
+						end
+					end
+				end
+			end
 		end
 
 		describe "as wrong user" do
@@ -125,6 +155,7 @@ describe "Authentication" do
 				before { delete user_path(user) }
 				specify { expect(response).to redirect_to(root_url) }
 			end
+
 		end
 	end
 end
